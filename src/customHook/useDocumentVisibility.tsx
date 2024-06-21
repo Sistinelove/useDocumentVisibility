ecbc2dfe9b22750f0fe9628f48026db441625815
Sistinelove <142403330+Sistinelove@users.noreply.github.com>
2024-06-21 18:55:16 +0500

    update useDocumentVisibility добавлен lazy initial 2 useEffect Объеденены в один убрал из useEffect ssr

--- src/customHook/useDocumentVisibility.tsx
+++ src/customHook/useDocumentVisibility.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 export const useDocumentVisibility = () => {
   const isClientSide = typeof document !== "undefined";
-  const [count, setCount] = useState(0);
-  const [visible, setVisible] = useState(isClientSide ? !document.hidden : true);
-  const [handlers, setHandlers] = useState<((isVisible: boolean) => void)[]>([]);
+  const [count, setCount] = useState(() => 0);
+  const [visible, setVisible] = useState(() => (isClientSide ? !document.hidden : true));
+  const [handlers, setHandlers] = useState<((isVisible: boolean) => void)[]>(() => []);
 
   useEffect(() => {
     const handleVisibilityChange = () => {
+      const isVisible = !document.hidden;
       if (document.hidden) {
         setCount(prev => prev + 1);
         setVisible(false);
       } else {
         setVisible(true);
       }
+      handlers.forEach(handler => handler(isVisible));
     };
 
     document.addEventListener("visibilitychange", handleVisibilityChange);
@@ -21,20 +23,7 @@ export const useDocumentVisibility = () => {
     return () => {
       document.removeEventListener("visibilitychange", handleVisibilityChange);
     };
-  }, [isClientSide]);
-
-  useEffect(() => {
-    const handleVisibility = () => {
-      const isVisible = !document.hidden;
-      handlers.forEach(handler => handler(isVisible));
-    };
-
-    document.addEventListener("visibilitychange", handleVisibility);
-
-    return () => {
-      document.removeEventListener("visibilitychange", handleVisibility);
-    };
-  }, [handlers]);
+  }, []);
 
   const onVisibilityChange = useCallback((handler: (isVisible: boolean) => void) => {
     setHandlers(prevHandlers => [...prevHandlers, handler]);
@@ -44,5 +33,5 @@ export const useDocumentVisibility = () => {
     };
   }, []);
 
-  return { count, visible, onVisibilityChange };
+  return {count, visible, onVisibilityChange};
 };
